5ed01b91f0fdf25bc34ce624f1556bcf2c1356b5
Jim Knochelmann <jim.knochelmann@ibm.com>
2022-08-24 21:23:48 -0600

    build 1.11 images

--- buildAllVersions.sh
+++ buildAllVersions.sh
@@ -5,8 +5,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Todo: this should be refactored to run as a matrix of github actions env vars
 
 ${SCRIPT_DIR}/buildAndPush.sh '1.11.0-gpu' '1.11.0+cu115'
-${SCRIPT_DIR}/buildAndPush.sh '1.13.0-gpu' '1.11.0+cu115'
 ${SCRIPT_DIR}/buildAndPush.sh '1.11.0-gpu' '1.12.0+cu116'
-${SCRIPT_DIR}/buildAndPush.sh '1.13.0-gpu' '1.12.0+cu116'
+
+# build separately due to space on device
+#${SCRIPT_DIR}/buildAndPush.sh '1.13.0-gpu' '1.11.0+cu115'
+#${SCRIPT_DIR}/buildAndPush.sh '1.13.0-gpu' '1.12.0+cu116'
 
 
